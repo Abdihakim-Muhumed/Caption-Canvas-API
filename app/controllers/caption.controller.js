4dc4e1eba9a11515fe2594322ec8e07d43f7d554
Abdihakim Muhumed <abdihakim.0017@gmail.com>
2024-06-16 18:44:42 +0300

    Added endpoint to add new photo caption

--- app/controllers/caption.controller.js
+++ app/controllers/caption.controller.js
@@ -19,8 +19,39 @@ const getPhotoCaptions = (req, res) => {
     })
 }
 
+const addNewCaption = (req, res) => {
+    const captionBody = req.query.captionBody;
+    const photoId = req.params.photoId;
+
+    Photo.findByPk(photoId)
+    .then(photo => {
+        if(!photo){
+            res.status(403).json({
+                message: "Cannot add caption to a non-existant photo"
+            })
+        }
+        Caption.create({
+            body: captionBody,
+            photoId: photoId
+        })
+        .then(caption => {
+            res.status(201).json({
+                caption: caption,
+                photo: photo
+            })
+        })
+        .catch( err => {
+            res.status(500).json({
+                message: err.message
+            })
+        })
+
+    })
+
+}
 
 
 module.exports = {
-    getPhotoCaptions
+    getPhotoCaptions,
+    addNewCaption,
 }
--- app/routes/caption.routes.js
+++ app/routes/caption.routes.js
@@ -4,5 +4,6 @@ const controller = require('../controllers/caption.controller');
 const captionRouter = express.Router();
 
 captionRouter.get('/:photoId', controller.getPhotoCaptions);
+captionRouter.post('/:photoId', controller.addNewCaption);
 
 module.exports = captionRouter
