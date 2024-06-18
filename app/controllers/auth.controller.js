1fe27eb81745f7ad9ed36f535eeee28833fcff65
Abdihakim Muhumed <abdihakim.0017@gmail.com>
2024-06-18 08:37:36 +0300

    Created signin endpoint

--- app/controllers/auth.controller.js
+++ app/controllers/auth.controller.js
@@ -3,6 +3,8 @@ const User = db.user;
 const Role = db.role;
 const Op = db.Sequelize.Op;
 const bcrypt = require('bcryptjs');
+const config = require('../config/auth.config')
+const jwt = require('jsonwebtoken');
 
 const signUp = (req, res) => {
     User.create({
@@ -16,7 +18,7 @@ const signUp = (req, res) => {
             Role.findAll({
                 where: {
                     name: {
-                        [Op.or]: req.query.likes
+                        [Op.or]: req.query.roles
                     }
                 }
             })
@@ -31,6 +33,11 @@ const signUp = (req, res) => {
                     res.status(500).json({
                         message: err.message
                     })
+                })
+            })
+            .catch(err => {
+                res.status(500).json({
+                    message: err.message
                 })
             })
         }
@@ -55,6 +62,71 @@ const signUp = (req, res) => {
     })
 }
 
+const signIn = (req, res) => {
+    User.findOne({
+        where: {
+            username: req.query.username
+        }
+    })
+    .then(user => {
+        if(!user){
+            res.status(403).json({
+                message: "Invalid username!"
+            })
+        }
+
+        const passwordIsValid = bcrypt.compareSync(
+            req.query.password,
+            user.password
+        )
+
+        if(!passwordIsValid){
+            res.status(403).json({
+                accessToken: null,
+                message: "Invalid Password!"
+            })
+        }
+
+        const token = jwt.sign(
+            {id: user.id},
+            config.secret,
+            {
+                algorithm:'HS256',
+                allowInsecureKeySizes: true,
+                expiresIn: 86400
+            }
+
+        )
+        let userRoles = [];
+        user.getRoles()
+        .then(roles => {
+            for(i=0; i<roles.length; i++){
+                userRoles.push("ROLE_" + roles[i].name.toUpperCase())
+            }
+            res.status(200).json({
+                id: user.id,
+                username: user.username,
+                fullName: user.fullName,
+                email: user.email,
+                roles: userRoles,
+                accessToken: token
+            })
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: err.message
+            })
+        })
+        
+    })
+    .catch(err => {
+        res.status(500).json({
+            message: err.message
+        })
+    })
+}
+
 module.exports = {
     signUp,
+    signIn
 }
--- app/routes/auth.routes.js
+++ app/routes/auth.routes.js
@@ -4,5 +4,6 @@ const controller = require('../controllers/auth.controller');
 const authRouter = express.Router();
 
 authRouter.post('/signup', controller.signUp);
+authRouter.post('/signin', controller.signIn);
 
 module.exports = authRouter;
