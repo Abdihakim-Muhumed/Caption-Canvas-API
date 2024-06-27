cdfde2559819f5c2ca8f5e0839558710859317b2
Abdihakim Muhumed <abdihakim.0017@gmail.com>
2024-06-27 10:37:17 +0300

    Added CORS

--- server.js
+++ server.js
@@ -4,9 +4,19 @@ const photoRouter = require('./app/routes/photo.routes');
 const captionRouter = require('./app/routes/caption.routes');
 const authRouter = require('./app/routes/auth.routes')
 const cookieParser = require('cookie-parser')
+const cors = require('cors')
+
 
 const app = express();
 
+const corsOption = {
+    origin: "http://localhost:3000",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    preflightContinue: false,
+    optionsSuccessStatus: 200
+}
+
+app.use(cors(corsOption))
 app.use(cookieParser());
 
 
